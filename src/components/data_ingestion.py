eb82d12cea7a5d15f74ddc30203f8ac0b1dafafb
shivakumar0002 <godipally.shivakumar01@gmail.com>
2024-01-12 20:22:49 -0500

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,8 @@
+import sys
+sys.path.insert(0, '/absolute/path/to/project/root')
 import os
 import sys
-from src.exception import CustomException
+from src.exception import CustomException 
 from src.logger import logging
 import pandas as pd
 
@@ -8,8 +10,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig 
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -23,7 +27,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('/Users/godipally.shivakumar/Desktop/Ml project/notebook/stud.csv')
+            df=pd.read_csv('notebook/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -52,8 +56,9 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data) 
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
